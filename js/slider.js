1cff92d82e78c76a5ebebe8bc76a075bf15d2724
Alex John Suarez <alexjohnsuarez@Alexs-MacBook-Pro.local>
2021-06-14 22:43:04 +0800

    Finish adding proper comments

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(ev){
-  // Preloader
+  /**
+   * Preloader
+   */
   document.querySelector(".preloader").style.display = "none";
   document.querySelector(".pages").style.display = "block";
 
-  // get next photo
+  /**
+   * Event on clicking next button
+   */
   document.querySelector(".next").addEventListener("click", function() {
     const current = next(images, selectedImageIndex);
 
@@ -11,7 +15,9 @@ document.addEventListener('DOMContentLoaded', function(ev){
       updateCanvasDataAndIndex(current)
   });
 
-  // get previous photo
+  /**
+   * Event on clicking previous button
+   */
   document.querySelector(".prev").addEventListener("click", function() {
     const current = previous(images, selectedImageIndex);
     
@@ -20,6 +26,9 @@ document.addEventListener('DOMContentLoaded', function(ev){
   });
 
   // delete photo
+  /**
+   * Event on deleting photo
+   */
   document.querySelector(".delete").addEventListener("click", function() {
     if (confirm("Are you sure you want to delete this photo?")) {
       const key = canvas.getAttribute("key");
@@ -32,14 +41,18 @@ document.addEventListener('DOMContentLoaded', function(ev){
 });
 
 
-// update html(canvas in photo, index and x of total) when pressing next, previous
+/**
+ * Update html(canvas in photo, index and x of total) when pressing next, previous
+ */
 const updateCanvasDataAndIndex = (selectedIndex) => {
   selectedImageIndex = selectedIndex;
   currentPhotoInCanvas(images[selectedIndex].data.file, images[selectedIndex].key);
   renderFromTo(images.length, selectedIndex);
 }
 
-// previous function
+/**
+ * Previous functionality
+ */
 const previous =  (images, current) => {
   if(images.length === 1 || current <= 0) 
     return current; 
@@ -47,7 +60,9 @@ const previous =  (images, current) => {
   return current -= 1;
 }
 
-// next function
+/**
+ * Next functionality
+ */
 const next = (images, current) =>  {
   if(images.length === 1 || current >= images.length - 1) 
     return current; 
@@ -55,7 +70,9 @@ const next = (images, current) =>  {
   return  current += 1;
 }
 
-// (current of total) function
+/**
+ * (current of total) function
+ */
 const renderFromTo = (total, current_photo, ) => {
   document.querySelector(".current").innerHTML = current_photo + 1;
   document.querySelector(".total").innerHTML = total;
